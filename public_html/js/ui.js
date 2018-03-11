34321875a54fcf2b19d1042416e388c8ea5826ab
Tim Zöller <tim.zoeller@ilume.de>
2018-03-11 22:41:54 +0100

    Some kind of styling

--- public_html/js/ui.js
+++ public_html/js/ui.js
@@ -1,6 +1,14 @@
 var UI = {};
 
 UI.updateCarDetail = function (error, car) {
-    document.querySelector('#car-result-vin').innerHTML = car[0];
-    document.querySelector('#car-result-kilometers').innerHTML = car[1];
+    document.querySelector('#car-result-vin').value = car[0];
+    document.querySelector('#car-result-kilometers').value = car[1];
+};
+
+UI.navigate = function (panelId) {
+    document.querySelectorAll('form.panel').forEach(function (panel) {
+        panel.className = 'panel hidden';
+    });
+    document.querySelector('#' + panelId).className = 'panel';
+
 };
